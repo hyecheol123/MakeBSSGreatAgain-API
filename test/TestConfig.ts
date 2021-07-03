d2d66f373067c35b1b2e3da7a924e052bfcdf157
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2021-07-04 02:15:11 +0900

    Test Environment

---
+++ test/TestConfig.ts
@@ -0,0 +1,60 @@
+/**
+ * Configuration for the Test Environment.
+ * Work identical as ServerConfig of src.
+ *
+ * @author Hyecheol (Jerry) Jang
+ */
+
+import * as crypto from 'crypto';
+import {ConfigObj} from '../src/datatypes/ConfigObj';
+import ServerConfigTemplate from '../src/ServerConfigTemplate';
+
+/**
+ * Module contains the configuration
+ */
+export default class TestConfig extends ServerConfigTemplate {
+  /**
+   * Constructor for ServerConfig
+   *
+   * @param identifier test name / used to identify test cases
+   */
+  constructor(identifier: string) {
+    const config: ConfigObj = {
+      db: {
+        url: 'localhost',
+        port: 3306,
+        username: 'apptest',
+        password: '',
+        defaultDatabase: `db_${identifier}`,
+      },
+      redis: {
+        host: 'localhost',
+        port: 6379,
+        user: '',
+        db: 14,
+      },
+      expressPort: 3000,
+      jwtKeys: {secretKey: 'keySecret', refreshKey: 'keySecret'},
+    };
+    super(config);
+  }
+
+  /**
+   * Function to create hashed password
+   *
+   * @param id user's id (used to generate salt)
+   * @param additionalSalt unique additional salt element for each user
+   * @param secretString string to be hashed (password, etc)
+   * @returns {string} Hashed Password
+   */
+  static hash(
+    id: crypto.BinaryLike,
+    additionalSalt: crypto.BinaryLike,
+    secretString: crypto.BinaryLike
+  ): string {
+    const salt: crypto.BinaryLike = id.toString() + additionalSalt.toString();
+    return crypto
+      .pbkdf2Sync(secretString, salt, 10, 64, 'sha512')
+      .toString('base64');
+  }
+}
